a049632894f2c65507008a556e7c8033e604cbef
Paulo Vieira Centeno <paulo.vieira.centeno@outlook.com>
2020-07-13 18:05:21 -0300

    Creating some html templates

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,17 +2,42 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
+	"text/template"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		//		http.NotFound(w, r)
+		w.Write([]byte(r.URL.Path))
 		return
 	}
 
-	w.Write([]byte("Hello from Snippetbox"))
+	// Initialize a slice containing the paths to the two files. Note that the
+	// home.page.tmpl file must be the *first* file in the slice.
+	files := []string{
+		"./ui/html/home.page.go.html",
+		"./ui/html/base.layout.go.html",
+		"./ui/html/footer.partial.go.html",
+	}
+
+	// Use the template.ParseFiles() function to read the files and store the
+	// templates in a template set. Notice that we can pass the slice of file paths
+	// as a variadic parameter?
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
