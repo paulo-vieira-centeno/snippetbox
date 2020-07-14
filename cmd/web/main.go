f61e8243bcc55ae442e2af03ea885834f1c3adbc
Paulo Vieira Centeno <paulo.vieira.centeno@outlook.com>
2020-07-14 17:28:45 -0300

    Centralizing error handling

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,6 +8,11 @@ import (
 	"path/filepath"
 )
 
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 
@@ -17,10 +22,16 @@ func main() {
 
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	// Initialize a new instance of application containing the dependencies.
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
 
 	//	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	//	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
