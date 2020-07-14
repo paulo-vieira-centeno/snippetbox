c03c4832c1bc756fd8c4e46529df79640252dbbe
Paulo Vieira Centeno <paulo.vieira.centeno@outlook.com>
2020-07-14 17:35:26 -0300

    Refactoring routes

---
+++ cmd/web/routes.go
@@ -0,0 +1,19 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
+
+	//	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	//	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	// Avoiding directory listing
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./static")})
+	mux.Handle("/static", http.NotFoundHandler())
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	return mux
+}
